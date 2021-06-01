c2d37c26f9d5b4c3dc992dc2bcb6f975e4ed7d32
John Medeiros <78372567+JMedeiros-dev@users.noreply.github.com>
2021-06-01 19:49:42 -0400

    Update settings.py

--- filmlist/settings.py
+++ filmlist/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '1iq5i@0r25(%=*9&r0jr44*bi@*d%)cwz+9rcqkk#ey1wh=ftq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
